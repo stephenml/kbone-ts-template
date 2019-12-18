56aaff1cef3ae56274b6d672463383229a25aaa4
Stephen <stephen.ml@hotmail.com>
2019-12-18 14:28:23 +0800

    添加云函数示例

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    Hello World
+    <div class="text">Hello World</div>
+    <button class="login-button" @click="login">Login</button>
   </div>
 </template>
 
@@ -11,11 +12,36 @@ import { Component, Provide, Vue } from 'vue-property-decorator'
   name: 'Home'
 })
 export default class Home extends Vue {
+  login () {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: (result: any) => {
+        console.log('[云函数] [login] user openid: ', result.result.openid)
+      },
+      fail: (error: any) => {
+        console.error('[云函数] [login] 调用失败', error)
+      }
+    })
+  }
 }
 </script>
 
 <style lang="scss">
 .home {
   text-align: center;
+
+  .text {
+    font-size: 26px;
+    margin-top: 200px;
+    margin-bottom: 20px;
+  }
+
+  .login-button {
+    border: 2px solid #07C160;
+    border-radius: 5px;
+    padding: 10px 20px;
+  }
 }
 </style>
